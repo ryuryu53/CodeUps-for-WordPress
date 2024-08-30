a38291b24b5e69e66b1525dadf70e719dab18e0e
ryu.ryu <ryumiya51+53@gmail.com>
2024-08-30 19:53:59 +0900

    お問い合わせフォームでよくあるミス 24.08.30

--- WordPressTheme/functions.php
+++ WordPressTheme/functions.php
@@ -67,3 +67,9 @@ function change_posts_per_page($query) {
   }
 }
 add_action( 'pre_get_posts', 'change_posts_per_page' );
+
+// Contact Form 7で自動挿入されるPタグ、brタグを削除
+add_filter('wpcf7_autop_or_not', 'wpcf7_autop_return_false');
+function wpcf7_autop_return_false() {
+  return false;
+}
